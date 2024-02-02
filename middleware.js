5905c13e40fcf035b538b5219e03816654b6331c
Zaki Munshi <zaki-munshi@hotmail.co.uk>
2024-02-02 21:46:05 +0000

    storeReturnTo update

--- middleware.js
+++ middleware.js
@@ -6,3 +6,10 @@ module.exports.isLoggedIn = (req, res, next) => {
     }
     next();
 }
+
+module.exports.storeReturnTo = (req, res, next) => {
+    if (req.session.returnTo) {
+        res.locals.returnTo = req.session.returnTo;
+    }
+    next();
+}
