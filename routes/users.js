9c68d583a37c29757cdebc7170d32fe0a1290464
Zaki Munshi <zaki-munshi@hotmail.co.uk>
2024-02-02 21:45:14 +0000

    returnTo update

--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const User = require('../models/user');
 const catchAsync = require('../utils/catchAsync');
 const passport = require('passport');
-
+const { storeReturnTo } = require('../middleware');
 
 router.get('/register', (req, res) => {
     res.render('users/register')
@@ -30,12 +30,14 @@ router.get('/login', (req, res) => {
     res.render('users/login');
 })
 
-router.post('/login', passport.authenticate('local', { failureFlash: true, failureRedirect: '/login' }), (req, res) => {
-    req.flash('success', 'welcome back!');
-    const redirectUrl = req.session.returnTo || '/campgrounds';
-    delete req.session.returnTo;
+router.post('/login',
+  storeReturnTo,
+  passport.authenticate('local', {failureFlash: true, failureRedirect: '/login'}),
+  (req, res) => {
+    req.flash('success', 'Welcome back!');
+    const redirectUrl = res.locals.returnTo || '/campgrounds';
     res.redirect(redirectUrl);
-})
+  });
 
 router.get('/logout', (req, res, next) => {
     req.logout(function (err) {
