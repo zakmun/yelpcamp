69c9ce82a6c2366bec46374a7fdb7a981ee86b3d
Zaki Munshi <zaki-munshi@hotmail.co.uk>
2024-02-03 11:23:58 +0000

    Adding author object id to file

--- seeds/index.js
+++ seeds/index.js
@@ -20,6 +20,7 @@ const seedDB = async () => {
         const random1000 = Math.floor(Math.random() * 1000);
         const price = Math.floor(Math.random() * 30) + 10;
         const camp = new Campground({
+            author: '65be2149f722433bdcc622e6',
             location: `${cities[random1000].city}, ${cities[random1000].state}`,
             title: `${sample(descriptors)} ${sample(places)}`,
             image: `https://source.unsplash.com/random/?camping,${i}`,
