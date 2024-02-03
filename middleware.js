25ab170381340b4db3770b8b6cc4a30a021d7ede
Zaki Munshi <zaki-munshi@hotmail.co.uk>
2024-02-03 12:28:02 +0000

    Code update

--- middleware.js
+++ middleware.js
@@ -40,6 +40,16 @@ module.exports.isAuthor = async (req, res, next) => {
      next();
 }
 
+module.exports.isReviewAuthor = async (req, res, next) => {
+    const {id, reviewId} = req.params;
+    const review = await Review.findById(reviewId);
+    if(!review.author.equals(req.user._id)){
+        req.flash('error', 'You do not have permission to do that!');
+        return res.redirect(`/camgrounds/${id}`);
+     }
+     next();
+}
+
 module.exports.validateReview = (req, res, next) => {
     const {error} = reviewSchema.validate(req.body);
     if(error) {
