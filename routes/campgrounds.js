4d570af01b870ac6638d207c96f43091180fcd6f
Zaki Munshi <zaki-munshi@hotmail.co.uk>
2024-02-03 11:29:08 +0000

    populating author name onto db

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -35,7 +35,7 @@ router.get("/", catchAsync(async (req, res) => {
  }));
  
  router.get("/:id", isLoggedIn, catchAsync(async (req, res) => {
-     const campground = await Campground.findById(req.params.id).populate('reviews');
+     const campground = await Campground.findById(req.params.id).populate('reviews').populate('author');
      if(!campground){
         req.flash('error', 'Campground does not exist!')
         return res.redirect('/campgrounds')
